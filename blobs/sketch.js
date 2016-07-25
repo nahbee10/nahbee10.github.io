f6794049e2e972cf2dce5076f2907dda22a488de
Nahee Kim <nahbee10@gmail.com>
2016-07-24 20:15:38 -0400

    hihi

--- blobs/sketch.js
+++ blobs/sketch.js
@@ -154,7 +154,7 @@ function setup() {
   }
 
   for (var i = 0; i < 16; i++) {
-    var spring1 = new VerletSpring2D(particles_tit_tit2[i], particles_tit_tit2[(i + 1) % particles_tit_tit2.length], 7, 0.1);
+    var spring1 = new VerletSpring2D(particles_tit_tit2[i], particles_tit_tit2[(i + 1) % particles_tit_tit2.length], 1, 0.01);
     springs.push(spring1);
     physics.addSpring(spring1);
     if (i % 1 == 0) {
