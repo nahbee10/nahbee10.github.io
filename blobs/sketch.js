c9db0dd9283d85bae93d76251490f1459c3cb881
Nahee Kim <nahbee10@gmail.com>
2016-07-24 22:20:03 -0400

    hihi

--- blobs/sketch.js
+++ blobs/sketch.js
@@ -16,26 +16,22 @@ var springs = [];
 var attractor;
 var attractor2;
 var capture;
-var tracker;
-var w = 640, h = 480;
+var motionHistoryImage;
+var repeler;
 
 function addNewBoob(boot, tit_1, tit_2) {
   
 }
 
 function setup() {
-  createCanvas(960, 720);
+
+  var w = 960, h = 720;
+
+  createCanvas(w, h);
 
   capture = createCapture(VIDEO);
-  capture.size(960, 720);
-  //capture.size(1100, 700);
+  capture.size(w, h);
   capture.hide();
-
-  colorMode(HSB);
-
-  tracker = new clm.tracker;
-  tracker.init(pModel);
-  tracker.start(capture.elt);
 
   // Initialize the physics
   physics = new VerletPhysics2D();
@@ -44,9 +40,9 @@ function setup() {
   for (var i = 0; i < 50; i++) {
     particles.push(new Particle(new Vec2D(random(width), random(height)), 4, 80, -1));
   }
-  /*for (var i = 0; i < 25; i++) {
+  for (var i = 0; i < 25; i++) {
     particles_tit.push(new Particle(new Vec2D(random(width), random(height)), 4, 80, -8));
-  }*/
+  }
   for (var i = 0; i < 10; i++) {
     particles_tit_tit.push(new Particle(new Vec2D(random(width), random(height)), 4, 80, -8));
   }
@@ -54,9 +50,9 @@ function setup() {
   for (var i = 0; i < 50; i++) {
     particles2.push(new Particle(new Vec2D(random(width), random(height)), 4, 80, -1));
   }
-  /*for (var i = 0; i < 25; i++) {
+  for (var i = 0; i < 25; i++) {
     particles_tit2.push(new Particle(new Vec2D(random(width), random(height)), 4, 80, -8));
-  }*/
+  }
   for (var i = 0; i < 10; i++) {
     particles_tit_tit2.push(new Particle(new Vec2D(random(width), random(height)), 4, 80, -8));
   }
@@ -85,7 +81,7 @@ function setup() {
 
   }
 
-  /*for (var i = 0; i < 25; i++) {
+  for (var i = 0; i < 25; i++) {
     var spring1 = new VerletSpring2D(particles_tit[i], particles_tit[(i + 1) % particles_tit.length], 1, 0.01);
     springs.push(spring1);
     physics.addSpring(spring1);
@@ -100,8 +96,7 @@ function setup() {
       physics.addSpring(spring2);
     }
 
-  }*/
-
+  }
 
   for (var i = 0; i < 10; i++) {
     var spring1 = new VerletSpring2D(particles_tit_tit[i], particles_tit_tit[(i + 1) % particles_tit_tit.length], 1, 0.1);
@@ -120,7 +115,7 @@ function setup() {
 
   }
 
-  for (var i = 0; i < 50; i++) {
+    for (var i = 0; i < 50; i++) {
     var spring1 = new VerletSpring2D(particles2[i], particles2[(i + 1) % particles2.length], 5, 0.01);
     springs.push(spring1);
     physics.addSpring(spring1);
@@ -137,7 +132,7 @@ function setup() {
 
   }
 
-  /*for (var i = 0; i < 25; i++) {
+  for (var i = 0; i < 25; i++) {
     var spring1 = new VerletSpring2D(particles_tit2[i], particles_tit2[(i + 1) % particles_tit2.length], 1, 0.01);
     springs.push(spring1);
     physics.addSpring(spring1);
@@ -152,7 +147,7 @@ function setup() {
       physics.addSpring(spring2);
     }
 
-  }*/
+  }
 
   for (var i = 0; i < 10; i++) {
     var spring1 = new VerletSpring2D(particles_tit_tit2[i], particles_tit_tit2[(i + 1) % particles_tit_tit2.length], 1, 0.1);
@@ -174,14 +169,98 @@ function setup() {
 
 }
 
+var backgroundPixels;
+function resetBackground(){
+  backgroundPixels = undefined;
+}
+
+var repelers = [];
+
 function draw() {
 
   //randomSeed(99);
   background(55);
+  /////////////for the motion history js part
 
-  image(capture, 0, 0, 960, 720);
+  image(capture, 0, 0);
+  capture.loadPixels)();
+  if(capture.pixels.length>0){
+    var w = capture.width, h = capture.height;
+    if(!backgroundPixels){
+      // copy the camera pixels for storing the background
+      backgroundPixels = copyImage(capture.pixels, backgroundPixels);
+      // make a grayscale image for storing the motion history
+      motionHistoryImage = new Uint8ClampedArray(w*h);
+    }
+    var pixels = capture.pixels;
+    var thresholdAmount = select('#thresholdAmount').value() / 100;
+    var sumSquaredThreshold = thresholdAmount * (255*255)*3;
+    var iRgb = 0, iGray = 0;
+    for (var y = 0; y < h; y++) {
+      for (var x = 0; x < w; x++) {
+        var rdiff = pixels[iRgb+0] - backgroundPixels[iRgb+0];
+        var gdiff = pixels[iRgb+1] - backgroundPixels[iRgb+1];
+        var bdiff = pixels[iRgb+2] - backgroundPixels[iRgb+2];
+        var sumSquaredDiff = rdiff * rdiff + gdiff * gdiff + bdiff * bdiff;
+        //if this is a foreground pixel
+        if(sumSquaredDiff > sumSquaredThreshold){
+          //set the motion history image to white
+          motionHistoryImage[iGray] = 255;
+        } else{
+          //otherwise make it fade towards black
+          motionHistoryImage[iGray]--;
+        }
+      };
+    };
 
-  var positions = tracker.getCurrentPosition();
+    //some parameteres for calculating the motion vectors
+    var stepSize = 16;
+    var radius = 8;
+    var maximumDiff = 8; //ignore big "motion edges"
+    var minimumValue = 245; //ignore very old values
+    var arrowWidth = .25;
+    stroke(255);
+    noFill();
+
+
+    // pre-processing some values outside the loop
+    var upOffset = -radius * w;
+    var downOffset = +radius * w;
+    var leftOffset = -radius;
+    var rightOffset = +radius;
+    var maximumLength = Math.sqrt(maximumDiff * maximumDiff *2);
+    var i_for_r = 0;
+    for(var y = radius; y + radius < h; y += stepSize){
+      for(var x = radius; x + radius < w; x += stepSize){
+        var i = y*w +x;
+        var center = motionHistoryImage[i];
+        var dx = 0, dy = 0;
+        if(center > minimumValue){
+          var up = motionHistoryImage[i+upOffset];
+          var down = motionHistoryImage[i+downOffset];
+          var left = motionHistoryImage[i+leftOffset];
+          var right = motionHistoryImage[i+rightOffset];
+
+          dx = right - left;
+          dy = down - up;
+          //ignore big "motion edges"
+          if(dx > maximumDiff || dy > maximumDiff || -dx > maximumDiff || - dy > maximumDiff){
+            dx = 0, dy = 0;
+          }else{
+            //big changes are slow motion, small changes are fast motion
+            /*var length = Math.sqrt(dx*dx + dy*dy);
+            var rescale = (maximumLength - length) / length;
+            dx *= rescale;
+            dy *= rescale;*/
+            repelers[i_for_r] = new Particle(new Vec2D(x, y), 100, 50, -4);
+            i_for_r++;
+          }
+        }
+      }
+    }
+  }
+
+  ////////////////
 
   seconds = millis() / 1000;
   // Update the physics world
@@ -189,21 +268,15 @@ function draw() {
 
   //attractor.display();
 
-  if(positions.length > 0){
-    attractor.set(positions[27][0]-150,positions[27][1]);
-    attractor2.set(positions[32][0]+150,positions[32][1]);
-  }else{
-    attractor.set((width/4),height/2);
-    attractor2.set(((width/4)*3),height/2);
-  }
+  attractor.set((width/4),height/2);
+  attractor2.set(((width/4)*3),height/2);
   
   repeler.set(mouseX,mouseY);
 
   noStroke();
 
 
-  //rgb-for-boob-fill(255,195,160);
-  fill(22,37,100);
+  fill(255,195,160);
   beginShape();
   for (var i = 0; i < particles.length; i++) {
     //particles[i].display();
@@ -215,8 +288,7 @@ function draw() {
   }
   endShape(CLOSE);
 
-  //rgb-for-boob-fill(255,195,160);
-  fill(22,37,100);
+  fill(138,73,77,120);
 
   beginShape();
   for (var i = 0; i < particles_tit.length; i++) {
@@ -229,8 +301,7 @@ function draw() {
   }
   endShape(CLOSE);
 
-  //rgb-for-tit-fill(138,73,77);
-  fill(356,47,54);
+  fill(138,73,77);
 
   beginShape();
   for (var i = 0; i < particles_tit_tit.length; i++) {
@@ -243,8 +314,7 @@ function draw() {
   }
   endShape(CLOSE);
 
-  //rgb-for-boob-fill(255,195,160);
-  fill(22,37,100);
+  fill(255,195,160);
   beginShape();
   for (var i = 0; i < particles2.length; i++) {
     //particles[i].display();
@@ -256,7 +326,7 @@ function draw() {
   }
   endShape(CLOSE);
 
-  fill(22,37,100);
+  fill(138,73,77,120);
 
   beginShape();
   for (var i = 0; i < particles_tit2.length; i++) {
@@ -269,8 +339,7 @@ function draw() {
   }
   endShape(CLOSE);
 
-  //rgb-for-tit-fill(138,73,77);
-  fill(356,47,54);
+  fill(138,73,77);
 
   beginShape();
   for (var i = 0; i < particles_tit_tit2.length; i++) {
